fddab947450455c745736ec1d1467672c68dd930
Stricker2 <101269390+Stricker2@users.noreply.github.com>
2022-06-02 01:31:08 -0400

    create JS sheet and timeblocks within HTML

---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+const saveButtonEl = document.getElementById('saveBtn')
+var textBoxEl = document.getElementById('textarea')
+
+// To-do:
+// Grab the current date through the moment.js
+// grab the live time and color-code the timeblocks based on live time
+// Make the HTML timeblocks 'inputs'
+// Make the save button functional to save entries to the localstorage
+// ensure saved events persist after saving
