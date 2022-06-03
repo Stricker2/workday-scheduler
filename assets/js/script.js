674e389698d1bf32064b87d241ac42a311be24e2
Stricker2 <101269390+Stricker2@users.noreply.github.com>
2022-06-03 02:02:51 -0400

    add live date to header and add input fields

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,28 @@
 const saveButtonEl = document.getElementById('saveBtn')
-var textBoxEl = document.getElementById('textarea')
+var inputBoxEl = document.getElementById('textarea')
+var inputBoxHourEl = document.getElementById('hour')
+var currentDate = document.getElementById('currentDay')
+var todayDate = moment().format("dddd MMMM Do, YYYY")
+console.log(todayDate);
+var timeHour = moment().format("h")
+console.log(timeHour);
+var timeHourAndMinute = moment().format("h:mm")
+console.log(timeHourAndMinute);
 
 // To-do:
 // Grab the current date through the moment.js
+var getDate = function() {
+    currentDate.innerText = todayDate
+};
+
+getDate();
+
 // grab the live time and color-code the timeblocks based on live time
+var timeblockStatus = function() {
+    if() {}
+};
+
+
 // Make the HTML timeblocks 'inputs'
 // Make the save button functional to save entries to the localstorage
 // ensure saved events persist after saving
