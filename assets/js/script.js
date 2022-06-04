ea55e1cfa77e3d3758c1663e0e4d30eab19c7b2c
Stricker2 <101269390+Stricker2@users.noreply.github.com>
2022-06-04 14:19:15 -0400

    color code time blocks based on live time

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,12 @@
 const saveButtonEl = document.getElementById('saveBtn')
-var inputBoxEl = document.getElementById('textarea')
+var inputBoxEl = document.getElementsByClassName('textarea')
 var inputBoxHourEl = document.getElementById('hour')
+var hourNumber = document.querySelectorAll('.timeblock')
 var currentDate = document.getElementById('currentDay')
 var todayDate = moment().format("dddd MMMM Do, YYYY")
-console.log(todayDate);
-var timeHour = moment().format("h")
-console.log(timeHour);
+var timeHour = parseInt(moment().hour())
 var timeHourAndMinute = moment().format("h:mm")
-console.log(timeHourAndMinute);
+
 
 // To-do:
 // Grab the current date through the moment.js
@@ -18,13 +17,34 @@ var getDate = function() {
 getDate();
 
 // grab the live time and color-code the timeblocks based on live time
-// var timeblockStatus = function() {
-//     if(inputBoxHourEl) {
-//         JSON.parse(timeHour) < inputBoxHourEl
-//     }
-// };
+var timeblockStatus = function() {
+    for(var i=0; i < hourNumber.length; i++) {
+        // var inputBoxEl = document.getElementsByClassName('textarea')
+        // console.log('inputBoxEl', inputBoxEl)
+        var blockHour = parseInt(hourNumber[i].children[0].getAttribute('id').split('-')[0]);
+        console.log('blockHour', blockHour);
+        console.log('numeric hour',timeHour)
+        var timeBox = hourNumber[i].children[1];
+        console.log('time box', timeBox)
+        // console.log('input box', inputBoxEl[i])
+        if(blockHour < timeHour) {
+            timeBox.classList.add('past')
+            timeBox.classList.remove('present')
+            timeBox.classList.remove('future')
+        } 
+        else if (blockHour === timeHour) {
+            timeBox.classList.remove('past')
+            timeBox.classList.add('present')
+            timeBox.classList.remove('future')
+        } 
+        else {
+            timeBox.classList.remove('past')
+            timeBox.classList.remove('present')
+            timeBox.classList.add('future')
+        }
+    }
+};
 
-
-// Make the HTML timeblocks 'inputs'
+timeblockStatus();
 // Make the save button functional to save entries to the localstorage
 // ensure saved events persist after saving
