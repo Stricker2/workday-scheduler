4dd7e1090ef34d6cfcb83046beecc55ae2104895
Stricker2 <101269390+Stricker2@users.noreply.github.com>
2022-06-05 00:46:04 -0400

    add ability to change text to 9am input textbox and save to localstorage

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,26 +7,19 @@ var todayDate = moment().format("dddd MMMM Do, YYYY")
 var timeHour = parseInt(moment().hour())
 var timeHourAndMinute = moment().format("h:mm")
 
-
-// To-do:
 // Grab the current date through the moment.js
 var getDate = function() {
     currentDate.innerText = todayDate
 };
-
 getDate();
 
 // grab the live time and color-code the timeblocks based on live time
 var timeblockStatus = function() {
     for(var i=0; i < hourNumber.length; i++) {
-        // var inputBoxEl = document.getElementsByClassName('textarea')
-        // console.log('inputBoxEl', inputBoxEl)
+        
         var blockHour = parseInt(hourNumber[i].children[0].getAttribute('id').split('-')[0]);
-        console.log('blockHour', blockHour);
-        console.log('numeric hour',timeHour)
         var timeBox = hourNumber[i].children[1];
-        console.log('time box', timeBox)
-        // console.log('input box', inputBoxEl[i])
+        
         if(blockHour < timeHour) {
             timeBox.classList.add('past')
             timeBox.classList.remove('present')
@@ -44,7 +37,43 @@ var timeblockStatus = function() {
         }
     }
 };
-
 timeblockStatus();
+
+var nineAMInput = document.getElementById('textarea-0').value = "My To-do"
+var currentNineAMInput = nineAMInput.nodeValue;
+$('#textarea-0').on('click', function() {
+    var text = $(this).val();
+    console.log(text);
+    
+    var textInput9AM = $('#textarea-0')
+    .val(text);
+    console.log(textInput9AM);
+
+    $(this).replaceWith(textInput9AM);
+
+    textInput9AM.trigger('focus');
+
+    $(textInput9AM).on('blur', function() {
+        console.log('new text has stayed');
+        var newText = $(this)
+            .val();
+        console.log(newText);
+    });
+
+    $('#saveBtn').on('click', function() {
+        localStorage.setItem('9AM task', JSON.stringify(textInput9AM.val()))
+    });
+    
+});
+
 // Make the save button functional to save entries to the localstorage
+
+
+
+
+
+
+
+
+
 // ensure saved events persist after saving
