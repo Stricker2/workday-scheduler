96ebbbb3a3ce5c02aa2539e36bca9de861a1f089
Stricker2 <101269390+Stricker2@users.noreply.github.com>
2022-06-05 18:04:58 -0400

    add edit and save jquery for other hours

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,7 @@
-const saveButtonEl = document.getElementById('saveBtn')
-var inputBoxEl = document.getElementsByClassName('textarea')
-var inputBoxHourEl = document.getElementById('hour')
 var hourNumber = document.querySelectorAll('.timeblock')
 var currentDate = document.getElementById('currentDay')
 var todayDate = moment().format("dddd MMMM Do, YYYY")
 var timeHour = parseInt(moment().hour())
-var timeHourAndMinute = moment().format("h:mm")
 
 // Grab the current date through the moment.js
 var getDate = function() {
@@ -59,11 +55,213 @@ $('#textarea-0').on('click', function() {
         console.log(newText);
     });
 
-    $('#saveBtn').on('click', function() {
+    $('#saveBtn-0').on('click', function() {
         localStorage.setItem('9AM task', JSON.stringify(textInput9AM.val()))
     });
     
 });
+
+$('#textarea-1').on('click', function() {
+    var text = $(this).val();
+    console.log(text);
+    
+    var textInput10AM = $('#textarea-1')
+    .val(text);
+    console.log(textInput10AM);
+
+    $(this).replaceWith(textInput10AM);
+
+    textInput10AM.trigger('focus');
+
+    $(textInput10AM).on('blur', function() {
+        
+        var newText = $(this)
+            .val();
+        console.log(newText);
+    });
+
+    $('#saveBtn-1').on('click', function() {
+        localStorage.setItem('10AM task', JSON.stringify(textInput10AM.val()))
+    });
+    
+});
+
+$('#textarea-2').on('click', function() {
+    var text = $(this).val();
+    console.log(text);
+    
+    var textInput11AM = $('#textarea-2')
+    .val(text);
+    console.log(textInput11AM);
+
+    $(this).replaceWith(textInput11AM);
+
+    textInput11AM.trigger('focus');
+
+    $(textInput11AM).on('blur', function() {
+        
+        var newText = $(this)
+            .val();
+        console.log(newText);
+    });
+
+    $('#saveBtn-2').on('click', function() {
+        localStorage.setItem('11AM task', JSON.stringify(textInput11AM.val()))
+    });
+    
+});
+
+$('#textarea-3').on('click', function() {
+    var text = $(this).val();
+    console.log(text);
+    
+    var textInput12PM = $('#textarea-3')
+    .val(text);
+    console.log(textInput12PM);
+
+    $(this).replaceWith(textInput12PM);
+
+    textInput12PM.trigger('focus');
+
+    $(textInput12PM).on('blur', function() {
+        
+        var newText = $(this)
+            .val();
+        console.log(newText);
+    });
+
+    $('#saveBtn-3').on('click', function() {
+        localStorage.setItem('12PM task', JSON.stringify(textInput12PM.val()))
+    });
+    
+});
+
+$('#textarea-4').on('click', function() {
+    var text = $(this).val();
+    console.log(text);
+    
+    var textInput1PM = $('#textarea-4')
+    .val(text);
+    console.log(textInput1PM);
+
+    $(this).replaceWith(textInput1PM);
+
+    textInput1PM.trigger('focus');
+
+    $(textInput1PM).on('blur', function() {
+        
+        var newText = $(this)
+            .val();
+        console.log(newText);
+    });
+
+    $('#saveBtn-4').on('click', function() {
+        localStorage.setItem('1PM task', JSON.stringify(textInput1PM.val()))
+    });
+    
+});
+
+$('#textarea-5').on('click', function() {
+    var text = $(this).val();
+    console.log(text);
+    
+    var textInput2PM = $('#textarea-5')
+    .val(text);
+    console.log(textInput2PM);
+
+    $(this).replaceWith(textInput2PM);
+
+    textInput2PM.trigger('focus');
+
+    $(textInput2PM).on('blur', function() {
+        
+        var newText = $(this)
+            .val();
+        console.log(newText);
+    });
+
+    $('#saveBtn-5').on('click', function() {
+        localStorage.setItem('2PM task', JSON.stringify(textInput2PM.val()))
+    });
+    
+});
+
+$('#textarea-6').on('click', function() {
+    var text = $(this).val();
+    console.log(text);
+    
+    var textInput3PM = $('#textarea-6')
+    .val(text);
+    console.log(textInput3PM);
+
+    $(this).replaceWith(textInput3PM);
+
+    textInput3PM.trigger('focus');
+
+    $(textInput3PM).on('blur', function() {
+        
+        var newText = $(this)
+            .val();
+        console.log(newText);
+    });
+
+    $('#saveBtn-6').on('click', function() {
+        localStorage.setItem('3PM task', JSON.stringify(textInput3PM.val()))
+    });
+    
+});
+
+$('#textarea-7').on('click', function() {
+    var text = $(this).val();
+    console.log(text);
+    
+    var textInput4PM = $('#textarea-7')
+    .val(text);
+    console.log(textInput4PM);
+
+    $(this).replaceWith(textInput4PM);
+
+    textInput4PM.trigger('focus');
+
+    $(textInput4PM).on('blur', function() {
+        
+        var newText = $(this)
+            .val();
+        console.log(newText);
+    });
+
+    $('#saveBtn-7').on('click', function() {
+        localStorage.setItem('4PM task', JSON.stringify(textInput4PM.val()))
+    });
+    
+});
+
+$('#textarea-8').on('click', function() {
+    var text = $(this).val();
+    console.log(text);
+    
+    var textInput5PM = $('#textarea-8')
+    .val(text);
+    console.log(textInput5PM);
+
+    $(this).replaceWith(textInput5PM);
+
+    textInput5PM.trigger('focus');
+
+    $(textInput5PM).on('blur', function() {
+        
+        var newText = $(this)
+            .val();
+        console.log(newText);
+    });
+
+    $('#saveBtn-8').on('click', function() {
+        localStorage.setItem('5PM task', JSON.stringify(textInput5PM.val()))
+    });
+    
+});
+
+
 
 // load saved inputs from localstorage
 var load9AMTasks = function() {
